7bcaa88089f7c0d13ead2e833bedad9b28e8934a
sasmithx <sasmithgit@gmail.com>
2026-04-23 19:22:37 +0530

    router completed

--- internal/server/router.go
+++ internal/server/router.go
@@ -1,1 +1,23 @@
 package server
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func NewRouter() *gin.Engine {
+
+	r := gin.Default()
+
+	r.GET("/health", func(c *gin.Context) {
+
+		c.JSON(http.StatusOK, gin.H{
+			"ok":     "true",
+			"status": "healthy",
+		})
+	})
+
+	return r
+
+}
